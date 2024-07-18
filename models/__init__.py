f02e39b62aaf30f839cd8e13182b9431f8a9cdb4
abdul1ah33 <study.abdullah2005@gmail.com>
2024-07-18 13:47:47 -0700

    sf

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from os import getenv
 
 storage_t = getenv('HBNB_TYPE_STORAGE')
--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime, timezone
 import sqlalchemy
